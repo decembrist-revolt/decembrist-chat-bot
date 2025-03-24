ed643a3f81c961c721350c278000ca910ab09663
AViDia <157277809+AViDia-Evgenika@users.noreply.github.com>
2025-03-24 19:39:04 +0300

    add restrict member

---
+++ DecembristChatBotSharp/Entity/RestrictMember.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DecembristChatBotSharp.Entity;
+
+public readonly record struct RestrictMember(
+    [property: BsonId] RestrictMember.CompositeId Id,
+    RestrictType RestrictType)
+{
+    public readonly record struct CompositeId(
+        long TelegramId,
+        long ChatId
+    );
+}
+
+[Flags]
+public enum RestrictType : short
+{
+    All = 0,
+    Text = 1,
+    Sticker = 2,
+    Link = 4,
+    Emoji = 8
+}
