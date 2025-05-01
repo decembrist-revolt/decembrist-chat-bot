42818bf85a81aaacf93b2e433d2dfba65b9a15d8
AViDia <157277809+AViDia-Evgenika@users.noreply.github.com>
2025-05-01 23:50:11 +0300

    exclude none

--- DecembristChatBotSharp/UtilsExtensions.cs
+++ DecembristChatBotSharp/UtilsExtensions.cs
@@ -152,7 +152,8 @@ public static class UtilsExtensions
         });
 
     public static BotCommand[] GetCommandsByLevel(this IEnumerable<ICommandHandler> handlers, CommandLevel level) =>
-        handlers.Where(handler => level.HasFlag(handler.CommandLevel))
+        handlers.Where(handler => handler.CommandLevel != CommandLevel.None)
+            .Where(handler => level.HasFlag(handler.CommandLevel))
             .Select(handler => new BotCommand(handler.Command, handler.Description))
             .OrderBy(command => command.Command)
             .ToArray();
