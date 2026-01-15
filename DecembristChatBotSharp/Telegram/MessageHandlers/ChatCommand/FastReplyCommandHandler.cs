83f2c740d5fde534938b098f6577d82cf07e1c17
AViDia <157277809+AViDia-Evgenika@users.noreply.github.com>
2026-01-16 02:48:38 +0300

    fast reply reply

--- DecembristChatBotSharp/Telegram/MessageHandlers/ChatCommand/FastReplyCommandHandler.cs
+++ DecembristChatBotSharp/Telegram/MessageHandlers/ChatCommand/FastReplyCommandHandler.cs
@@ -6,6 +6,7 @@ using LanguageExt.UnsafeValueAccess;
 using Serilog;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace DecembristChatBotSharp.Telegram.MessageHandlers.ChatCommand;
 
@@ -38,7 +39,65 @@ public class FastReplyCommandHandler(
         var (messageId, telegramId, chatId) = parameters;
 
         if (parameters.Payload is not TextPayload { Text: var text }) return unit;
+        var maybeReplyFileId = parameters.ReplyFileId;
+        var maybeReplyText = parameters.ReplyToMessageText;
+        var type = parameters.MessageType;
+        if (type == MessageType.Unknown)
+        {
+            return await HandleFastReply(text, chatId, messageId, telegramId);
+        }
 
+        if (type == MessageType.Text && maybeReplyText.IsSome)
+        {
+            return await HandleFastReplyNew(chatId, telegramId, text, messageId, maybeReplyText.ValueUnsafe(), type);
+        }
+
+        if (maybeReplyFileId.IsSome)
+        {
+            return await HandleFastReplyNew(chatId, telegramId, text, messageId, maybeReplyFileId.ValueUnsafe(), type);
+        }
+
+        return unit;
+    }
+
+    private async Task<Unit> HandleFastReplyNew(long chatId, long telegramId, string textMessage, int messageId,
+        string textReply,
+        MessageType type)
+    {
+        var maybeMessageAndReply = ParseMessageAndReplyNew(textMessage, textReply);
+
+        if (maybeMessageAndReply.IsNone)
+        {
+            return await Array(SendFastReplyHelp(chatId),
+                messageAssistance.DeleteCommandMessage(chatId, messageId, Command)).WhenAll();
+        }
+
+        var (message, reply) = maybeMessageAndReply.ValueUnsafe();
+        var isAdmin = await adminUserRepository.IsAdmin((telegramId, chatId));
+        if (isAdmin && message == ChatCommandHandler.DeleteSubcommand)
+        {
+            return await DeleteFastReply(chatId, reply, messageId);
+        }
+
+        var maybeFastReply = await CreateFastReplyNew(chatId, telegramId, message, reply, messageId, type);
+
+        var taskFastReply = maybeFastReply.MapAsync(async fastReply =>
+        {
+            var result = await memberItemService.UseFastReply(chatId, telegramId, fastReply, isAdmin);
+            return result switch
+            {
+                UseFastReplyResult.NoItems => await messageAssistance.SendNoItems(chatId),
+                UseFastReplyResult.Blocked => await SendBlocked(chatId),
+                UseFastReplyResult.Duplicate => await SendDuplicateMessage(chatId, fastReply.Id.Message),
+                UseFastReplyResult.Success => await SendNewFastReply(chatId, fastReply.Id.Message, fastReply.Reply),
+                _ => unit
+            };
+        }).IfSome(identity);
+        return await Array(taskFastReply, expiredMessageRepository.QueueMessage(chatId, messageId)).WhenAll();
+    }
+
+    private async Task<Unit> HandleFastReply(string text, long chatId, int messageId, long telegramId)
+    {
         var args = text.Trim().Split(ArgSeparator).Skip(1).ToArray();
         var maybeMessageAndReply = ParseMessageAndReply(args);
         if (maybeMessageAndReply.IsNone)
@@ -119,6 +178,38 @@ public class FastReplyCommandHandler(
         return new FastReply((chatId, message), telegramId, reply, expireAt, messageType, replyType);
     }
 
+    private async Task<Option<FastReply>> CreateFastReplyNew(
+        long chatId, long telegramId, string message, string reply, int messageId, MessageType type)
+    {
+        var messageType = FastReplyType.Text;
+        if (message.StartsWith(FastReplyHandler.StickerPrefix))
+        {
+            var fileId = message[FastReplyHandler.StickerPrefix.Length..];
+            if (!await CheckSticker(chatId, fileId, messageId)) return None;
+
+            message = fileId;
+            messageType = FastReplyType.Sticker;
+        }
+        else message = message.ToLowerInvariant();
+
+        var replyType = type switch
+        {
+            MessageType.Sticker => FastReplyType.Sticker,
+            MessageType.Photo => FastReplyType.Photo,
+            MessageType.Animation => FastReplyType.Animation,
+            MessageType.Text => FastReplyType.Text,
+            _ => FastReplyType.Text
+        };
+        if (replyType != FastReplyType.Text)
+        {
+            if (!await CheckFile(chatId, reply, messageId, replyType)) return None;
+        }
+
+        var expireAt = DateTime.UtcNow.AddDays(appConfig.CommandConfig.FastReplyDaysDuration);
+
+        return new FastReply((chatId, message), telegramId, reply, expireAt, messageType, replyType);
+    }
+
     private async Task<bool> CheckSticker(long chatId, string fileId, int messageId)
     {
         if (await botClient.SendSticker(chatId, fileId).ToTryAsync().IsFail())
@@ -131,6 +222,20 @@ public class FastReplyCommandHandler(
         }
 
         return true;
+    }
+
+    private async Task<bool> CheckFile(long chatId, string fileId, int messageId, FastReplyType type)
+    {
+        var isExist = type switch
+        {
+            FastReplyType.Sticker => await botClient.SendSticker(chatId, fileId).ToTryAsync().IsSucc(),
+            FastReplyType.Photo => await botClient.SendPhoto(chatId, fileId).ToTryAsync().IsSucc(),
+            FastReplyType.Animation => await botClient.SendAnimation(chatId, fileId).ToTryAsync().IsSucc(),
+            _ => false
+        };
+        if (isExist) return true;
+        await messageAssistance.SendMessage(chatId, $"Медиа не найдено {fileId}", Command);
+        return false;
     }
 
     private async Task<Unit> SendNewFastReply(long chatId, string message, string reply)
@@ -171,7 +276,23 @@ public class FastReplyCommandHandler(
         if (message.StartsWith(_botUsername)) message = message[_botUsername.Length..];
         message = message.Trim();
 
-        if (string.IsNullOrWhiteSpace(message))
+        if (string.IsNullOrWhiteSpace(message) || string.IsNullOrWhiteSpace(reply))
+        {
+            return None;
+        }
+
+        return (message, reply);
+    }
+
+    private Option<(string, string)> ParseMessageAndReplyNew(string text, string reply)
+    {
+        var message = text.Trim().Split(ArgSeparator).Skip(1).First();
+        if (string.IsNullOrWhiteSpace(message)) return None;
+
+        if (message.StartsWith(_botUsername)) message = message[_botUsername.Length..];
+        message = message.Trim();
+
+        if (string.IsNullOrWhiteSpace(message) || string.IsNullOrWhiteSpace(reply))
         {
             return None;
         }
