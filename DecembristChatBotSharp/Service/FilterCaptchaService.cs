2111f986e8818901e5e14d65f19ba184637e6f2c
AViDia <157277809+AViDia-Evgenika@users.noreply.github.com>
2026-03-16 09:42:44 +0300

    replace filter service

---
+++ DecembristChatBotSharp/Service/FilterCaptchaService.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.RegularExpressions;
+using DecembristChatBotSharp.Entity;
+using DecembristChatBotSharp.Mongo;
+using DecembristChatBotSharp.Telegram.MessageHandlers;
+using Serilog;
+
+namespace DecembristChatBotSharp.Service;
+
+public class FilterCaptchaService(
+    AppConfig appConfig,
+    WhiteListRepository whiteListRepository,
+    FilterRecordRepository filterRecordRepository,
+    AdminUserRepository adminUserRepository,
+    DeepSeekService deepSeekService)
+{
+    private const int MaxMessageLength = 6;
+
+    private static readonly Regex LinkRegex = new(@"([^\s<>]+\.[^\s<>]{2,})",
+        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    private readonly string _scamTraitors =
+        "Признаки скама, достаточно одного:\n - " + string.Join("\n - ", appConfig.FilterJobConfig.ScamTraitors) + "\n";
+
+    private const string ModeratorPrompt = """
+                                           Ты - модератор. Ты определяешь, является ли сообщение спамом, мошенничеством (скамом) или рекламой,
+                                           Ответь СТРОГО в формате JSON, без дополнительного текста:
+                                           если скам: {"isScam": true}, иначе: {"isScam": false}
+                                           """;
+
+    public async Task<bool> IsSuspectMessage(ChatMessageHandlerParams parameters)
+    {
+        var (messageId, telegramId, chatId) = parameters;
+        if (await whiteListRepository.IsWhiteListMember((telegramId, chatId)) ||
+            await adminUserRepository.IsAdmin((telegramId, chatId))) return false;
+
+        if (parameters.Payload is not TextPayload { IsLink: var isLink, Text: var text })
+            return true;
+        if (isLink || LinkRegex.IsMatch(text) || !await IsFiltered(text, chatId))
+            return true;
+
+        return await IsSuspectFromAiModeration(chatId, telegramId, messageId, text, parameters.ReplyToMessageText);
+    }
+
+    private async Task<bool> IsSuspectFromAiModeration(long chatId, long telegramId, int messageId, string text,
+        Option<string> maybeReplyText)
+    {
+        var messageToCheck = $"Сообщение для проверки:\n\"{text}\"";
+        if (maybeReplyText.TryGetSome(out var replyText))
+        {
+            messageToCheck = $"""
+                              Ответ на сообщение:
+                              "{replyText}"
+                              {messageToCheck}
+                              """;
+        }
+
+        var prompt = ModeratorPrompt + _scamTraitors + messageToCheck;
+
+        var maybeVerdict = await deepSeekService.GetModerateVerdict(prompt, chatId, telegramId);
+        if (!maybeVerdict.TryGetSome(out var isScam))
+        {
+            Log.Error("Ai Moderation is fail, no action to user");
+            return true;
+        }
+
+        if (isScam) return true;
+
+        await whiteListRepository.AddWhiteListMember(new WhiteListMember(new CompositeId(telegramId, chatId)));
+        return false;
+    }
+
+    private async Task<bool> IsFiltered(string text, long chatId)
+    {
+        if (text.Length > MaxMessageLength) return true;
+        return await filterRecordRepository.IsFilterRecordContain(chatId, text);
+    }
+}
--- DecembristChatBotSharp/Telegram/MessageHandlers/FilteredMessageHandler.cs
+++ DecembristChatBotSharp/Telegram/MessageHandlers/FilteredMessageHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using DecembristChatBotSharp.Entity;
+﻿using DecembristChatBotSharp.Entity;
 using DecembristChatBotSharp.Entity.Configs;
 using DecembristChatBotSharp.Mongo;
 using DecembristChatBotSharp.Service;
@@ -12,42 +11,18 @@ namespace DecembristChatBotSharp.Telegram.MessageHandlers;
 
 [Singleton]
 public class FilteredMessageHandler(
-    WhiteListRepository whiteListRepository,
-    FilterRecordRepository filterRecordRepository,
     FilteredMessageRepository filteredMessageRepository,
-    DeepSeekService deepSeekService,
     BotClient botClient,
     AppConfig appConfig,
-    AdminUserRepository adminUserRepository,
+    FilterCaptchaService filterCaptchaService,
     CancellationTokenSource cancelToken,
     ChatConfigService chatConfigService)
 {
-    private const int MaxMessageLength = 6;
-
-    private static readonly Regex LinkRegex = new(@"([^\s<>]+\.[^\s<>]{2,})",
-        RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-    private readonly string _scamTraitors =
-        "Признаки скама, достаточно одного:\n - " + string.Join("\n - ", appConfig.FilterJobConfig.ScamTraitors) + "\n";
-
-    private const string ModeratorPrompt = """
-                                           Ты - модератор. Ты определяешь, является ли сообщение спамом, мошенничеством (скамом) или рекламой,
-                                           Ответь СТРОГО в формате JSON, без дополнительного текста:
-                                           если скам: {"isScam": true}, иначе: {"isScam": false}
-                                           """;
-
     public async Task<bool> Do(ChatMessageHandlerParams parameters)
     {
         var (messageId, telegramId, chatId) = parameters;
-        if (await whiteListRepository.IsWhiteListMember((telegramId, chatId)) ||
-            await adminUserRepository.IsAdmin((telegramId, chatId))) return false;
-
-        if (parameters.Payload is not TextPayload { IsLink: var isLink, Text: var text })
-            return await SendCaptchaMessage(chatId, messageId, telegramId);
-        if (isLink || LinkRegex.IsMatch(text) || !await IsFiltered(text, chatId))
-            return await SendCaptchaMessage(chatId, messageId, telegramId);
-
-        return await CheckAiModeration(chatId, telegramId, messageId, text, parameters.ReplyToMessageText);
+        if(!await filterCaptchaService.IsSuspectMessage(parameters)) return false;
+        return await SendCaptchaMessage(chatId, messageId, telegramId);
     }
 
     private async Task<bool> SendCaptchaMessage(long chatId, int messageId, long telegramId)
@@ -64,6 +39,7 @@ public class FilteredMessageHandler(
         var messageText = string.Format(
             filterConfig.CaptchaMessage, filterConfig.CaptchaAnswer, appConfig.FilterJobConfig.CaptchaTimeSeconds,
             appConfig.FilterJobConfig.CaptchaTryCount - tryCount);
+        
         return await botClient.SendMessage(chatId, messageText,
                 replyParameters: new ReplyParameters { MessageId = messageId },
                 cancellationToken: cancelToken.Token)
@@ -81,39 +57,5 @@ public class FilteredMessageHandler(
                     Log.Error(ex, "Failed to create filtered message in chat {0}, author: {1}", chatId, telegramId);
                     return false;
                 });
-    }
-
-    private async Task<bool> CheckAiModeration(long chatId, long telegramId, int messageId, string text,
-        Option<string> maybeReplyText)
-    {
-        var messageToCheck = $"Сообщение для проверки:\n\"{text}\"";
-        if (maybeReplyText.TryGetSome(out var replyText))
-        {
-            messageToCheck = $"""
-                              Ответ на сообщение:
-                              "{replyText}"
-                              {messageToCheck}
-                              """;
-        }
-
-        var prompt = ModeratorPrompt + _scamTraitors + messageToCheck;
-
-        var maybeVerdict = await deepSeekService.GetModerateVerdict(prompt, chatId, telegramId);
-        if (!maybeVerdict.TryGetSome(out var isScam))
-        {
-            Log.Error("Ai Moderation is fail, no action to user");
-            return await SendCaptchaMessage(chatId, messageId, telegramId);
-        }
-
-        if (isScam) return await SendCaptchaMessage(chatId, messageId, telegramId);
-
-        await whiteListRepository.AddWhiteListMember(new WhiteListMember(new CompositeId(telegramId, chatId)));
-        return false;
-    }
-
-    private async Task<bool> IsFiltered(string text, long chatId)
-    {
-        if (text.Length > MaxMessageLength) return true;
-        return await filterRecordRepository.IsFilterRecordContain(chatId, text);
     }
 }
